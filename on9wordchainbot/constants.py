4e18b7a331118bf99c9d8e0d9c38d16f09c290f0
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-08-02 04:10:17 +0700

    Update constants.py

--- on9wordchainbot/constants.py
+++ on9wordchainbot/constants.py
@@ -10,16 +10,16 @@ logger.info("Loading constants from config file")
 with open(filename) as f:
     config = json.load(f)
 
-TOKEN = config["6272942112:AAF9Y6U0B2ZyQ2hdsQ9D_ETT8VtlhzXiDO4"]    #5036197906:AAEs-DDN0X_DCa8K66zYQsXkz5w8GSaFgtI
-ON9BOT_TOKEN = config["6226805699:AAE1T96RKSjs06kNID7VW9xVsYaSOFDI92o"]   #5093671528:AAEpbDvCfJdb2ZFK1i9rSHupGqoZTHC1sI4
-DB_URI = config["mongodb+srv://minhquang24071996:2407@coihaycoc.1an4oky.mongodb.net/?retryWrites=true&w=majority"]   #mongodb+srv://agarta01:44malatya22@cluster0.advkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+TOKEN = config["TOKEN"]
+ON9BOT_TOKEN = config["ON9BOT_TOKEN"]
+DB_URI = config["DB_URI"]
 PROVIDER_TOKEN = config["PROVIDER_TOKEN"]
-OWNER_ID = config["5838967403"]  #5038665445
-ADMIN_GROUP_ID = config["-1001854415088"]   #-1001793811732
-OFFICIAL_GROUP_ID = config["-1001854415088"]  #-1001793811732
-WORD_ADDITION_CHANNEL_ID = config["-1001854415088"]   #-1001602923043
-VIP = config["coihaycoc"]   #Dnztrmn
-VIP_GROUP = config["coihaycoc247"]   #Keyfialemsohbet
+OWNER_ID = config["OWNER_ID"]
+ADMIN_GROUP_ID = config["ADMIN_GROUP_ID"]
+OFFICIAL_GROUP_ID = config["OFFICIAL_GROUP_ID"]
+WORD_ADDITION_CHANNEL_ID = config["WORD_ADDITION_CHANNEL_ID"]
+VIP = config["VIP"]
+VIP_GROUP = config["VIP_GROUP"]
 
 WORDLIST_SOURCE = "https://raw.githubusercontent.com/tmq247/noitutest/main/src/assets/tudien.txt"    #https://raw.githubusercontent.com/dwyl/english-words/master/words.txt
 
