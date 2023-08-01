724a0234e614050b5593287f1ca52eee38aeaf4f
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-08-01 13:21:39 +0700

    Update words.py

--- on9wordchainbot/words.py
+++ on9wordchainbot/words.py
@@ -17,7 +17,7 @@ class Words:
     @staticmethod
     async def update() -> None:
         # Words retrieved from online repo and database table with additional approved words
-        logger.info("Retrieving words")
+        logger.info("Lấy từ")
 
         async def get_words_from_source() -> List[str]:
             from . import session
@@ -29,14 +29,14 @@ class Words:
             from . import pool
 
             async with pool.acquire() as conn:
-                res = await conn.fetch("SELECT word from wordlist WHERE accepted;")
+                res = await conn.fetch("CHỌN từ từ danh sách từ NƠI được chấp nhận;")
                 return [row[0] for row in res]
 
         source_task = asyncio.create_task(get_words_from_source())
         db_task = asyncio.create_task(get_words_from_db())
         wordlist = await source_task + await db_task
 
-        logger.info("Processing words")
+        logger.info("Đang xử lý từ")
 
         wordlist = [w.lower() for w in wordlist if w.isalpha()]
         Words.dawg = CompletionDAWG(wordlist)
